8fa687eb18d671c45ea6e9e915f9aaaffe17cc0c
Matthew A. Russell <matthew@zaffra.com>
2011-01-27 17:53:30 -0600

    Refactoring to pull out parameters. Adding stderr logging

--- recipe__oauth_login.py
+++ recipe__oauth_login.py
@@ -1,11 +1,35 @@
 # -*- coding: utf-8 -*-
 
+import os
+import sys
 import twitter
 
 from twitter.oauth import write_token_file, read_token_file
 from twitter.oauth_dance import oauth_dance
 
-def oauth_login():
+def oauth_login(app_name, 
+                consumer_key, 
+                consumer_secret, 
+                token_file='out/twitter.oauth'):
+
+    try:
+        (access_token, access_token_secret) = read_token_file(token_file)
+    except IOError, e:
+        (access_token, access_token_secret) = oauth_dance(app_name, consumer_key,
+                consumer_secret)
+
+        if not os.path.isdir('out'):
+            os.mkdir('out')
+
+        write_token_file(token_file, access_token, access_token_secret)
+
+        print >> sys.stderr, "OAuth Success. Token file stored to", token_file
+
+    return twitter.Twitter(domain='api.twitter.com', api_version='1',
+                        auth=twitter.oauth.OAuth(access_token, access_token_secret,
+                        consumer_key, consumer_secret))
+
+if __name__ == '__main__':
 
     # Go to http://twitter.com/apps/new to create an app and get these items
     # See also http://dev.twitter.com/pages/oauth_single_token
@@ -13,19 +37,5 @@ def oauth_login():
     APP_NAME = ''
     CONSUMER_KEY = ''
     CONSUMER_SECRET = ''
-    TOKEN_FILE = 'out/twitter.oauth'
 
-    try:
-        (access_token, access_token_secret) = read_token_file(TOKEN_FILE)
-    except IOError, e:
-        (access_token, access_token_secret) = oauth_dance(APP_NAME, CONSUMER_KEY,
-                CONSUMER_SECRET)
-
-        if not os.path.isdir('out'):
-            os.mkdir('out')
-
-        write_token_file(TOKEN_FILE, access_token, access_token_secret)
-         
-    return twitter.Twitter(domain='api.twitter.com', api_version='1',
-                        auth=twitter.oauth.OAuth(access_token, access_token_secret,
-                        CONSUMER_KEY, CONSUMER_SECRET))
+    oauth_login(APP_NAME, CONSUMER_KEY, CONSUMER_SECRET)
