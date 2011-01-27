d2c6b79198de15cff9de86018954fe1b9e086776
Matthew A. Russell <matthew@zaffra.com>
2011-01-27 17:53:50 -0600

    Writing out directories to out/ and stderr logging

--- recipe__trending_topics_time_series.py
+++ recipe__trending_topics_time_series.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 import datetime
 import time
 import json
@@ -8,16 +9,20 @@ import twitter
 
 t = twitter.Twitter(domain='api.twitter.com', api_version='1')
 
-if not os.path.isdir('trends_data'):
-        os.mkdir('trends_data')
+if not os.path.isdir('out/trends_data'):
+        os.makedirs('out/trends_data')
 
 while True:
+
     now = str(datetime.datetime.now())
 
     trends = json.dumps(t.trends(), indent=1)
 
-    f = open(os.path.join(os.getcwd(), 'trends_data', now), 'w')
+    f = open(os.path.join(os.getcwd(), 'out', 'trends_data', now), 'w')
     f.write(trends)
     f.close()
+
+    print >> sys.stderr, "Wrote data file", f.name
+    print >> sys.stderr, "Zzz..."
 
     time.sleep(60) # 60 seconds
