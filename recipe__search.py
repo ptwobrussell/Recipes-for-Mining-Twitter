b205edb58b372b6ad0bf9fca309bb0f902f08bee
Matthew A. Russell <matthew@zaffra.com>
2011-01-27 19:35:03 -0600

    Fixing error in wrapping search_results in an extra list

--- recipe__search.py
+++ recipe__search.py
@@ -10,8 +10,10 @@ MAX_PAGES = 15
 RESULTS_PER_PAGE = 100
 
 twitter_search = twitter.Twitter(domain="search.twitter.com")
+
 search_results = []
 for page in range(1,MAX_PAGES+1):
-    search_results += [ twitter_search.search(q=Q, rpp=RESULTS_PER_PAGE, page=page)['results'] ]
+    search_results += \
+        twitter_search.search(q=Q, rpp=RESULTS_PER_PAGE, page=page)['results']
 
 print json.dumps(search_results, indent=1)
