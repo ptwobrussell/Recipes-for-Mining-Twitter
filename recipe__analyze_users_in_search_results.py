4a0a05e6ffb77af583981f38c7c4eeb5a070edee
Matthew A. Russell <matthew@zaffra.com>
2011-01-28 02:38:33 -0600

    more smoothing out

--- recipe__analyze_users_in_search_results.py
+++ recipe__analyze_users_in_search_results.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
 
+import sys
 import twitter
-from recipe__get_info import getInfoByScreenName
+from recipe__get_user_info import get_info_by_screen_name
 from recipe__oauth_login import oauth_login
 
-def analyzeUsersInSearchResults(q, max_pages=15, results_per_page=100):
+def analyze_users_in_search_results(t, q, max_pages=15, results_per_page=100):
 
     # Search for something
 
-    twitter_search = twitter.Twitter(domain="search.twitter.com")
+    search_api = twitter.Twitter(domain="search.twitter.com")
     search_results = []
     for page in range(1,max_pages+1):
-        search_results += twitter_search.search(q=q, rpp=results_per_page, page=page)['results']
-
-    twitter_api = oauth_login()
+        search_results += search_api.search(q=q, rpp=results_per_page, page=page)['results']
 
     # Extract the screen names (the "from_user" field) from the results
     # and optionally map them to a useful field like the tweet id
@@ -33,7 +32,7 @@ def analyzeUsersInSearchResults(q, max_pages=15, results_per_page=100):
 
     # Use the /users/lookup resource to resolve profile information for these screen names
 
-    screen_name_to_info = getInfoByScreenName(twitter_api, screen_name_to_tweet_ids.keys())
+    screen_name_to_info = get_info_by_screen_name(t, screen_name_to_tweet_ids.keys())
 
     # Extract the home location for each user. Note that the "location" field can be anything
     # a user has typed in, and may be something like "Everywhere", "United States" or something else
@@ -45,3 +44,16 @@ def analyzeUsersInSearchResults(q, max_pages=15, results_per_page=100):
     # the people who appear in the search results.
 
     return screen_name_to_info, screen_name_to_location, screen_name_to_tweet_ids
+
+if __name__ == '__main__':
+
+    Q = ' '.join(sys.argv[1:])
+
+    # Don't forget to pass in keyword parameters if you don't have
+    # a token file stored to disk
+
+    t = oauth_login()
+
+    sn2info, sn2location, sn2tweet_ids = analyze_users_in_search_results(t, Q)
+
+    # Go off and do interesting things...
