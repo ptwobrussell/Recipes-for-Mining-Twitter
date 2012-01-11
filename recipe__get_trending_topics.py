e2baf8677465819321e33dbc96ef6e487cf222ef
Matthew A. Russell <matthew@zaffra.com>
2012-01-10 21:45:52 -0600

    Updating broken recipe due to Twitter API change earlier this year. See also https://github.com/ptwobrussell/Mining-the-Social-Web/issues/15

--- recipe__get_trending_topics.py
+++ recipe__get_trending_topics.py
@@ -5,4 +5,9 @@ import twitter
 
 t = twitter.Twitter(domain='api.twitter.com', api_version='1')
 
-print json.dumps(t.trends(), indent=1)
+WORLD_WOE_ID = 1 # The Yahoo! Where On Earth ID for the entire world
+
+world_trends = t.trends._(WORLD_WOE_ID) # get back a callable
+
+# call the callable and iterate through the trends returned
+print json.dumps([ trend for trend in world_trends()[0]['trends'] ], indent=1) 
